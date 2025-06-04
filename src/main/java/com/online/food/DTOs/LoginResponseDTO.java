28c72c5bd287e96069796b26eb9123eac9855cc3
meet-gg <meetgoyanip@gmail.com>
2025-06-04 19:33:09 +0530

    add LoginResponseDTO

---
+++ src/main/java/com/online/food/DTOs/LoginResponseDTO.java
@@ -0,0 +1,16 @@
+package com.online.food.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponseDTO {
+    private Long userId;
+    private String accessToken;
+    private String refreshToken;
+}
